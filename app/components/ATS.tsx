0863fd5d48446b9383d6ad3dd6c7d50aee7e510d
EldridgeJr <eldridgeprincejr@gmail.com>
2025-11-29 22:13:57 +0100

    Implement wipe functionality

--- app/components/ATS.tsx
+++ app/components/ATS.tsx
@@ -33,10 +33,10 @@ const ATS = ({
                 <p className="text-2xl font-semibold">ATS Score - {score}/100</p>
             </div>
             <div className="flex flex-col gap-2">
-                <p className="font-medium text-xl">
+                <p className="font-medium text-lg">
                     How well does your resume pass through Applicant Tracking Systems?
                 </p>
-                <p className="text-xl text-gray-500">
+                <p className="text-lg text-gray-500">
                     Your resume was scanned like an employer would. Here's how it
                     performed:
                 </p>
@@ -51,7 +51,7 @@ const ATS = ({
                             alt="ATS"
                             className="w-4 h-4"
                         />
-                        <p className="text-lg text-gray-500">{suggestion.tip}</p>
+                        <p className="text-base text-gray-500">{suggestion.tip}</p>
                     </div>
                 ))}
                 <p className="font-medium text-lg text-gray-500">
