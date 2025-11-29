6bf879353e4ad7c7485a320d677d62ae34b1f9f8
EldridgeJr <eldridgeprincejr@gmail.com>
2025-11-29 18:32:49 +0100

    Resume feedback components

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -72,12 +72,12 @@ const Resume = () => {
                         </div>
                     )}
                 </section>
-                <section>
+                <section className="feedback-section">
                     <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
                     {feedback ? (
                         <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
                             <Summary feedback={feedback} />
-                            <ATS score={feedback.ATS.score || 0} suggestion={feedback.ATS.tips || []} />
+                            <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
                             <Details feedback={feedback} />
                         </div>
                     ) : (
